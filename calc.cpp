6eb87baa259a67a29a2509757c6d7c6240bc1738
Zach Saunders <zachisaunders12@gmail.com>
2024-06-07 21:40:30 -0400

    Making some progress

--- calc.cpp
+++ calc.cpp
@@ -1,15 +1,131 @@
 #include "calc.h"
 #include "./ui_calc.h"
 
+
 calc::calc(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::calc)
 {
+    runningTotal = 0;
+    inputBuffer  = 0;
+    nextIsDec    = false;
+    std::vector<double> prevInputsBuffer;
+    prevInputsBuffer.reserve(2);
+    std::map<char, std::vector<int>> operationIndicies;
+
     ui->setupUi(this);
+}
+
+int calc::appendInputBuff(int num) {
+    inputBuffer *= 10;
+    inputBuffer += num;
+    ui->lcdNumber->display(inputBuffer);
+    return inputBuffer;
 }
 
 calc::~calc()
 {
     delete ui;
+}
+
+void calc::on_oneButton_clicked()
+{
+    calc::appendInputBuff(1);
+}
+
+
+void calc::on_twobutton_clicked()
+{
+    calc::appendInputBuff(2);
+}
+
+
+void calc::on_threeButton_clicked()
+{
+    calc::appendInputBuff(3);
+}
+
+
+void calc::on_fourButton_clicked()
+{
+    calc::appendInputBuff(4);
+}
+
+
+void calc::on_fiveButton_clicked()
+{
+    calc::appendInputBuff(5);
+}
+
+
+void calc::on_sixButton_clicked()
+{
+    calc::appendInputBuff(6);
+}
+
+
+void calc::on_sevenButton_clicked()
+{
+    calc::appendInputBuff(7);
+}
+
+
+void calc::on_eightButton_clicked()
+{
+    calc::appendInputBuff(8);
+}
+
+
+void calc::on_nineButton_clicked()
+{
+    calc::appendInputBuff(9);
+}
+
+
+void calc::on_zeroButton_clicked()
+{
+    calc::appendInputBuff(0);
+}
+
+
+void calc::on_decButton_clicked()
+{
+    calc::appendInputBuff()
+}
+
+
+void calc::on_eqButton_clicked()
+{
+
+}
+
+
+void calc::on_plusButton_clicked()
+{
+
+}
+
+
+void calc::on_minusButton_clicked()
+{
+
+}
+
+
+void calc::on_multButton_clicked()
+{
+
+}
+
+
+void calc::on_divButton_clicked()
+{
+
+}
+
+
+void calc::on_lcdNumber_overflow()
+{
+    ui->lcdNumber->setDigitCount(ui->lcdNumber->digitCount() + 1);
 }
 
--- calc.h
+++ calc.h
@@ -2,6 +2,8 @@
 #define CALC_H
 
 #include <QMainWindow>
+#include <vector>
+#include <map>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -19,7 +21,47 @@ public:
 
 private slots:
 
+    void on_oneButton_clicked();
+
+    void on_twobutton_clicked();
+
+    void on_threeButton_clicked();
+
+    void on_fourButton_clicked();
+
+    void on_fiveButton_clicked();
+
+    void on_sixButton_clicked();
+
+    void on_sevenButton_clicked();
+
+    void on_eightButton_clicked();
+
+    void on_nineButton_clicked();
+
+    void on_zeroButton_clicked();
+
+    void on_decButton_clicked();
+
+    void on_eqButton_clicked();
+
+    void on_plusButton_clicked();
+
+    void on_minusButton_clicked();
+
+    void on_multButton_clicked();
+
+    void on_divButton_clicked();
+
+    void on_lcdNumber_overflow();
+
 private:
+    double  runningTotal;
+    double  inputBuffer;
+    bool    nextIsDec;
+    std::vector<double> prevInputsBuffer;
+    std::map<char, std::vector<int>> operationIndicies;
     Ui::calc *ui;
+    int appendInputBuff(int num);
 };
 #endif // CALC_H
